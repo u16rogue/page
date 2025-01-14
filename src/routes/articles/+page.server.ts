dcf18f201be457d22b5b88bf8ce85fa5104393ff
u16rogue <u16rogue@proton.me>
2025-01-15 01:06:44 +0800

    articles listing

--- src/routes/articles/+page.server.ts
+++ src/routes/articles/+page.server.ts
@@ -42,10 +42,18 @@ async function load_metadata(path: string): Promise<{ ok: true, value: ArticleMe
   return { ok: true, value: metadata };
 }
 
+type ProcessedArticleEntry = {
+  value: ArticleMetadata,
+  path: string,
+};
+
 export const load: PageServerLoad = async function (event) {
   if (!fs.existsSync(ARTICLES_DIR)) {
     throw "Articles directory not found.";
   }
+
+  const sorted_articles:   Array<ProcessedArticleEntry> = [];
+  const unsorted_articles: Array<ProcessedArticleEntry> = [];
 
   for (const dir of fs.readdirSync(ARTICLES_DIR)) {
     const dir1 = path.join(ARTICLES_DIR, dir);
@@ -54,10 +62,37 @@ export const load: PageServerLoad = async function (event) {
       continue;
     }
 
-
+    const processed_entry = { value: metadata.value, path: dir };
+    if (metadata.value.stamps?.edited || metadata.value.stamps?.added || metadata.value.stamps?.created) {
+      sorted_articles.push(processed_entry);
+    } else {
+      unsorted_articles.push(processed_entry);
+    }
   }
 
+  sorted_articles.sort(function (a, b) {
+    const aa = a.value.stamps?.edited || a.value.stamps?.added || a.value.stamps?.created;
+    const bb = b.value.stamps?.edited || b.value.stamps?.added || b.value.stamps?.created;
+
+    if (!aa || !bb) {
+      throw 'timestamp on sorted_articles should never result to nil';
+    }
+
+    if (aa == bb) {
+      return 0;
+    } else if (aa > bb) {
+      return 1;
+    } else if (aa < bb) {
+      return -1;
+    }
+
+    throw 'unsortable date. filter failed';
+  });
+
+  sorted_articles.push(...unsorted_articles);
+
   return {
+    articles: sorted_articles,
     _meta: {
       nav: {
         display: {
--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -1,11 +1,69 @@
 <script lang="ts">
+    import { base } from "$app/paths";
   import type { PageData } from "./$types";
   export let data: PageData;
 </script>
 
 <div class="main">
-  <p>articles pages</p>
+  {#each data.articles as article}
+    <div class="article-entry">
+
+      {#if article.value.thumbnail}
+        <div class="article-entry-img" style="background-image: url({article.value.thumbnail});"></div>
+      {:else}
+        <div class="article-entry-img">
+          <p class="text-nothumb">No Thumbnail</p>
+        </div>
+      {/if}
+
+      <div class="article-entry-info">
+        <a class="article-title" href="{base}/articles/{article.path}">{article.value.title || article.path}</a>
+        <p>{article.value.description || 'No description.'}</p>
+      </div>
+    </div>
+  {/each}
 </div>
 
 <style>
+  .main {
+    display: flex;
+    flex-direction: column;
+    padding: 12px;
+  }
+
+  .article-title {
+    color: var(--color-3);
+    font-size: 1.4rem;
+    font-weight: bolder;
+  }
+
+  .article-entry-info {
+    margin-left: 8px;
+  }
+
+  .article-entry-img {
+    aspect-ratio: 1;
+    height: 100px;
+    border-style: dotted;
+    border-color: var(--color-3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    background-position: center;
+    background-size: cover;
+  }
+
+  .article-entry {
+    border-left-style: solid;
+    border-width: 6px;
+    border-color: var(--color-3);
+    padding: 8px;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: fit-content;
+  }
 </style>
